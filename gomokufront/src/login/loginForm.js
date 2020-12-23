b80d32193af9075a7e7bd2f3c4a2e9ffdc04c607
maximus <1091261998@qq.com>
2020-12-23 17:23:17 +0800

    仿照火狐登录页面，使用flex布局，做一个登录页面

---
+++ gomokufront/src/login/loginForm.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import {Form,Input,Button,Checkbox} from "antd";
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+
+const LoginForm = () => {
+    const onFinish = (values) => {
+        console.log('Received values of form: ', values);
+    }
+
+    return (
+        <div>
+            <div className={"loginTips"}>
+                请登录
+            </div>
+            <Form
+                name="normal_login"
+                className="login-form"
+                initialValues={{
+                    remember: true,
+                }}
+                onFinish={onFinish}
+            >
+                <Form.Item
+                    name="username"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your Username!',
+                        },
+                    ]}
+                >
+                    <Input prefix={<UserOutlined className="site-form-item-icon" />}
+                           placeholder="请输入用户名" />
+                </Form.Item>
+                <Form.Item
+                    name="password"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your Password!',
+                        },
+                    ]}
+                >
+                    <Input.Password
+                        prefix={<LockOutlined className="site-form-item-icon" />}
+                        type="password"
+                        placeholder="请输入密码"
+                    />
+                </Form.Item>
+                <Form.Item>
+                    <Form.Item name="remember" valuePropName="checked" noStyle>
+                        <Checkbox>记住我</Checkbox>
+                    </Form.Item>
+
+                    <a className="login-form-forgot" href="">
+                        忘记密码
+                    </a>
+                </Form.Item>
+
+                <Form.Item>
+                    <Button type="primary" htmlType="submit" className="login-form-button">
+                        登录
+                    </Button>
+                    或者 <a href="">注册！</a>
+                </Form.Item>
+            </Form>
+        </div>
+    )
+}
+
+export default LoginForm
