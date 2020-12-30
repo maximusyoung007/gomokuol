71a0d04eaeefe68d3a70a4f076fbaffbe648217a
maximus <1091261998@qq.com>
2020-12-30 17:02:37 +0800

    棋盘鼠标位置显示、落子位置显示

--- gomokufront/src/game/play/board.js
+++ gomokufront/src/game/play/board.js
@@ -1,10 +1,12 @@
-import React,{useState} from "react"
+import React from "react"
 
 class Board extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            onBoard: -1
+            onBoard: -1,
+            clickTurn: 1,
+            havePut: Array(625).fill(null),
         }
     }
 
@@ -27,33 +29,124 @@ class Board extends React.Component {
             context.stroke();
         }
 
-        for(let i = 0;i < 15;i++) {
-            for(let j = 0;j < 15;j++) {
+        let points = [
+            [3,3],[3,11],[11,3],[11,11],[7,7]
+        ]
+        for(let i = 0;i < 5;i++) {
+            let point = points[i];
+            context.beginPath();
+            let x = 22 + 44 * point[0];
+            let y = 22 + 44 * point[1];
+            let radius = 3;
+            let startAngle = 0;
+            let endAngle = Math.PI + (Math.PI * 2) / 2;
+            let anticlockwise = true;
+            context.arc(x,y,radius,startAngle,endAngle,anticlockwise);
+            context.fill();
+        }
+    }
 
+    moveOn(e) {
+        this.setState({onBoard:1});
+    }
+
+    moveOut(e) {
+        this.setState({onBoard: -1});
+    }
+
+    drawIndex(ctx,indexX,indexY,color) {
+        ctx.beginPath();
+        ctx.moveTo(indexX - 10, indexY - 4);
+        ctx.lineTo(indexX - 4, indexY - 4);
+        ctx.lineTo(indexX - 4, indexY - 10);
+        ctx.strokeStyle = color;
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(indexX + 4, indexY - 10);
+        ctx.lineTo(indexX + 4, indexY - 4);
+        ctx.lineTo(indexX + 10, indexY - 4);
+        ctx.strokeStyle = color;
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(indexX - 10, indexY + 4);
+        ctx.lineTo(indexX - 4, indexY + 4);
+        ctx.lineTo(indexX - 4, indexY + 10);
+        ctx.strokeStyle = color;
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.moveTo(indexX + 4, indexY + 10);
+        ctx.lineTo(indexX + 4, indexY + 4);
+        ctx.lineTo(indexX + 10, indexY + 4);
+        ctx.strokeStyle = color;
+        ctx.stroke();
+    }
+
+    whereToMove(e) {
+        const havePut = this.state.havePut;
+        if(this.state.onBoard == 1) {
+            const c = document.getElementById("board");
+            let ctx = c.getContext("2d");
+            let x = e.pageX - c.getBoundingClientRect().left;
+            let y = e.pageY - c.getBoundingClientRect().top;
+            for (let i = 0; i < 15; i++) {
+                for (let j = 0; j < 15; j++) {
+                    let indexX = 22 + 44 * i;
+                    let indexY = 22 + 44 * j;
+                    let s = indexX + "," + indexY;
+                    if (havePut.indexOf(s) == -1) {
+                        if (Math.abs(x - indexX) < 22 && Math.abs(y - indexY) < 22) {
+                            console.log(indexX + "---" + indexY);
+                            this.drawIndex(ctx,indexX,indexY,"red");
+                        } else {
+                            this.drawIndex(ctx,indexX,indexY,"white");
+                        }
+                    }
+                }
             }
         }
     }
 
-    toMove(e) {
-        this.onBoard = 1;
-    }
-
-    moveOut(e) {
-        this.onBoard = -1;
-    }
-
-    whereToMove(e) {
-        const c = document.getElementById("board");
-        var x = e.pageX - c.getBoundingClientRect().left;
-        var y = e.pageY - c.getBoundingClientRect().top;
-    }
-
-
-
-    constructor(props) {
-        super(props);
-        this.state = {
-
+    putOn(e) {
+        if(this.state.onBoard == 1) {
+            const havePut = this.state.havePut;
+            const c = document.getElementById("board");
+            let ctx = c.getContext("2d");
+            let x = e.pageX - c.getBoundingClientRect().left;
+            let y = e.pageY - c.getBoundingClientRect().top;
+            for (let i = 0; i < 15; i++) {
+                for (let j = 0; j < 15; j++) {
+                    let indexX = 22 + 44 * i;
+                    let indexY = 22 + 44 * j;
+                    let s = indexX + "," + indexY;
+                    if(havePut.indexOf(s) == -1) {
+                        if (Math.abs(x - indexX) < 22 && Math.abs(y - indexY) < 22) {
+                            ctx.beginPath();
+                            let radius = 22;
+                            let startAngle = 0;
+                            let endAngle = Math.PI + (Math.PI * 2) / 2;
+                            let anticlockwise = true;
+                            ctx.arc(indexX, indexY, radius, startAngle, endAngle, anticlockwise);
+                            if (this.state.clickTurn == 1) {
+                                ctx.strokeStyle = "black";
+                                ctx.fillStyle = "black";
+                            } else {
+                                ctx.strokeStyle = "black";
+                                ctx.fillStyle = "white";
+                            }
+                            this.setState({clickTurn: this.state.clickTurn * (-1)});
+                            ctx.fill();
+                            ctx.stroke();
+                            havePut.push(s);
+                            this.setState({
+                                havePut: havePut
+                            })
+                        }
+                    }
+                }
+            }
         }
     }
 
@@ -61,9 +154,10 @@ class Board extends React.Component {
         return(
             <div>
                 <canvas id="board"
-                        onMouseOver={this.toMove}
-                        onMouseDown={this.moveOut}
-                        onMouseMove={this.whereToMove}
+                        onMouseOver={(event) => this.moveOn(event)}
+                        onMouseOut={(event) => this.moveOut(event)}
+                        onMouseMove={(event) => this.whereToMove(event)}
+                        onClick={(event) => this.putOn(event)}
                         width={660}
                         height={660}></canvas>
             </div>
