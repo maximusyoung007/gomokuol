2ce67f1570343570e7bfb0e0371190ee873d348e
maximus <1091261998@qq.com>
2021-01-26 17:34:12 +0800

    好友页面布局

--- gomokufront/src/game/friends/friends.js
+++ gomokufront/src/game/friends/friends.js
@@ -1,11 +1,50 @@
 import React from "react"
+import { Input, List, Avatar } from 'antd'
+
+require("../game.css");
+
+const data = [
+  {
+    title: '虚假的好友 1',
+  },
+  {
+    title: '虚假的好友 2',
+  },
+  {
+    title: '虚假的好友 3',
+  },
+  {
+    title: '虚假的好友 4',
+  },
+];
 
 const Friends = () => {
-    return(
-        <div>
-            friends
-        </div>
-    )
+  const {Search} = Input;
+  const onSearch = value => {
+    console.log(value);
+  }
+  return(
+    <div className={"friends"}>
+      <div className={"friendsList"}>
+        <Search placeholder="input search text" onSearch={onSearch} enterButton />
+        <List
+          itemLayout="horizontal"
+          dataSource={data}
+          renderItem={item => (
+            <List.Item>
+
+              <Avatar shape={"square"} src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+              <span style={{float: "left"}}>{item.title}</span>
+
+            </List.Item>
+          )}
+        />
+      </div>
+      <div className={"friendInfo"}>
+
+      </div>
+    </div>
+  )
 }
 
 export default Friends
