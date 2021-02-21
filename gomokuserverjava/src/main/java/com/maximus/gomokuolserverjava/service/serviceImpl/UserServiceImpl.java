bbe68f79dbf87f245fef7f034460b4f5708eb9d7
maximus <1091261998@qq.com>
2021-02-22 00:07:30 +0800

    一些配置

--- gomokuserverjava/src/main/java/com/maximus/gomokuolserverjava/service/serviceImpl/UserServiceImpl.java
+++ gomokuserverjava/src/main/java/com/maximus/gomokuolserverjava/service/serviceImpl/UserServiceImpl.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.extension.service.IService;
 import com.maximus.gomokuolserverjava.entity.User;
 import com.maximus.gomokuolserverjava.mapper.UserMapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.maximus.gomokuolserverjava.service.UserService;
 import org.springframework.stereotype.Service;
 
 /**
@@ -15,6 +16,6 @@ import org.springframework.stereotype.Service;
  * @since 2021-02-21
  */
 @Service
-public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IService<User> {
+public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements UserService {
 
 }
