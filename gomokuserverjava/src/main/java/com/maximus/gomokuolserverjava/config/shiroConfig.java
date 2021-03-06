29d4e9526ad15ff40bc2662580bb3891cf31c06b
maximus <1091261998@qq.com>
2021-03-06 21:01:57 +0800

    。

---
+++ gomokuserverjava/src/main/java/com/maximus/gomokuolserverjava/config/shiroConfig.java
@@ -0,0 +1,8 @@
+package com.maximus.gomokuolserverjava.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class shiroConfig {
+
+}
---
+++ gomokuserverjava/src/main/java/com/maximus/gomokuolserverjava/realm/DbShiroRealm.java
@@ -0,0 +1,9 @@
+package com.maximus.gomokuolserverjava.realm;
+
+import com.maximus.gomokuolserverjava.service.UserService;
+
+public class DbShiroRealm {
+    DbShiroRealm(UserService userService) {
+
+    }
+}
